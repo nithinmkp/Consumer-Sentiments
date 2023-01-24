a6df343348b61b065a9075b2516884ec6d7d5848
(nithinmkp) <write2nithinm@gmail.com>
2023-01-24 19:11:51 +0530

    small update to generalICS part code

--- Codes/01-Data-wrangling.R
+++ Codes/01-Data-wrangling.R
@@ -25,14 +25,24 @@ dat_list<-readRDS(here(clean_master_data,"datlist.Rdata")) # Master Data
 ### General ICS ----
 
 #### Data Transformation ----
-dat_list_gen<-map(dat_list,data_transform_fn,
-              pivot_cols = 10:14)
+dat_list_gen<-dat_list |> 
+        reduce(bind_rows) |> 
+        data_transform_fn(pivot_cols = 10:14)
 
 ### Category-Wise ICS ----
 
 #### Data Transformation ----
 
 cat_dat_list<-dat_list |> 
+        reduce(bind_rows) |> 
+        group_vars_fn(var_select = c(6,15:20),
+                      group_var = Series)
+
+### State-Wise ICS ----
+
+#### Data Transformation ----
+
+state_dat_list<-dat_list |> 
         reduce(bind_rows) |> 
         group_vars_fn(var_select = c(6,15:20),
                       group_var = Series)
@@ -44,9 +54,8 @@ cat_dat_list<-dat_list |>
 
 ## General ICS ----
 ### Components of ICS and ICS ----
-dat_ICS<-map(dat_list_gen,ind_calculate_fn)|> 
-        map_df(bind_rows) |> 
-        arrange(month_slot) |> 
+dat_ICS<-dat_list_gen |>
+        ind_calculate_fn() |> 
         rowwise() |> 
         mutate(ICS=mean(c_across(2:6)))
 
